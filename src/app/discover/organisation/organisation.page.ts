1c5982ec2002cc06a33c702275b02457e87e7fee
Francis Adu-Gyamfi <francis.adu-gyamfi@topgolf.com>
2023-01-29 00:24:46 +0000

    Adding missing page files

---
+++ src/app/discover/organisation/organisation.page.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Organisation } from '../../shared/models/api/organisation';
+import { OrganisationService } from '../../shared/services/api/organisation.service';
+
+@Component({
+  selector: 'app-organisation',
+  templateUrl: './organisation.page.html',
+  styleUrls: ['./organisation.page.scss'],
+})
+export class OrganisationPage implements OnInit {
+
+  public organisation: Organisation;
+
+  constructor(
+    public organisationService: OrganisationService,
+    public route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    this.loadOrganisation();
+  }
+
+  async loadOrganisation() {
+    this.organisation = this.organisationService.getSelectedModel();
+
+    if( this.organisation ) {
+      return;
+    }
+
+    const slug = this.route.snapshot.paramMap.get('slug');
+    this.organisationService.getBySlug(slug).subscribe({
+      next: (organisation: Organisation) => this.organisation = organisation
+    });
+  }
+}
